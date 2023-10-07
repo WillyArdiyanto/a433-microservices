b8db471bc6e7d5eccfcf1b4db312d18ceb81f8ad
Vincentius Willy Ardiyanto <72459873+WillyArdiyanto@users.noreply.github.com>
2023-10-07 15:03:23 +0700

    Update build_push_image.sh

--- build_push_image.sh
+++ build_push_image.sh
@@ -1,15 +1,15 @@
 # Buat image Docker dari Dockerfile dari kriteria 2
 docker build -t item-app:v1 .
 
-export CR_PAT = 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
+export CR_PAT=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
 # Lihat daftar image
 docker images
 
 # Ubah nama image agar sesuai dengan format Github Packages
-docker image tag item-app:v1 ghcr.io/WillyArdiyanto/item-app:v1
+docker image tag item-app:v1 ghcr.io/willyardiyanto/item-app:v1
 
 # Login ke Github Packages
 echo $CR_PAT | docker login ghcr.io -u WillyArdiyanto --password-stdin
 
 # Unggah image ke Docker Hub
-docker push ghcr.io/WillyArdiyanto/item-app:v1
+docker push ghcr.io/willyardiyanto/item-app:v1
